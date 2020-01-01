864b6d84a954f64b02a2006a83574f221ee03981
joshtyf <tan.yin.feng@hotmail.com>
2020-01-01 16:04:05 +0800

    added bootstrap cdn

--- app/javascript/components/Body.jsx
+++ app/javascript/components/Body.jsx
@@ -57,6 +57,12 @@ class Body extends React.Component {
   render() {
     return (
       <div>
+        <link
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossOrigin="anonymous"
+        ></link>
         <h1>Task Manager</h1>
         <Search handleChange={this.handleChange} />
         <br />
