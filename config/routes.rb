9682896563137ea31a9320df32cfcf9bafd777fa
joshtyf <tan.yin.feng@hotmail.com>
2020-01-12 19:05:24 +0800

    added old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'welcome/index'
   root 'welcome#index'
+  get 'show', to: 'tasks#show'
+  post 'create', to: 'tasks#create'
+  delete 'delete/:id(.:format)', to: 'tasks#destroy'
+  put 'update/:id(.:format)', to: 'tasks#update'
+  post 'search', to: 'tasks#search'
+  get 'categories', to: 'categories#show'
+  post 'filter', to: 'tasks#filter'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
