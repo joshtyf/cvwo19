7129d99b87c9358e0c1d7c3494c60021fb9616d8
joshtyf <tan.yin.feng@hotmail.com>
2020-01-13 14:21:11 +0800

    replaced bootstrap dependency with cdn

--- app/javascript/components/Body.js
+++ app/javascript/components/Body.js
@@ -4,7 +4,6 @@ import AllTasks from "./AllTasks";
 import NewTask from "./NewTask";
 import axios from "axios";
 import Search from "./Search";
-import "bootstrap/dist/css/bootstrap.min.css";
 import SideBar from "./SideBar";
 
 const csrfToken = document.querySelector('[name="csrf-token"]').content;
