51b80df95ae6336f7dc9b76cb7b428d89c80c7cd
joshtyf <tan.yin.feng@hotmail.com>
2020-01-17 15:12:32 +0800

    testing sendgrid (2)

--- app/javascript/components/Body.js
+++ app/javascript/components/Body.js
@@ -99,25 +99,16 @@ class Body extends React.Component {
     // };
     // var response = SendGrid.send(msg);
     // console.log(response);
-    var helper = require("sendgrid").mail;
-    var from_email = new helper.Email("e0406483@u.nus.edu");
-    var to_email = new helper.Email("e0406483@u.nus.edu");
-    var subject = "Hello World from the SendGrid Node.js Library!";
-    var content = new helper.Content("text/plain", "Hello, Email!");
-    var mail = new helper.Mail(from_email, subject, to_email, content);
-
-    var sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: "POST",
-      path: "/v3/mail/send",
-      body: mail.toJSON()
-    });
-
-    sg.API(request, function(error, response) {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
+    const sgMail = require("@sendgrid/mail");
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: "e0406483@u.nus.edu",
+      from: "e0406483@u.nus.edu",
+      subject: "Sending with Twilio SendGrid is Fun",
+      text: "and easy to do anywhere, even with Node.js",
+      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+    };
+    sgMail.send(msg);
   }
 
   render() {
