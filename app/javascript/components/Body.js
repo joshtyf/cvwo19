98d1809ed5fd09161e63fc3004bb38f35e0c46c4
joshtyf <tan.yin.feng@hotmail.com>
2020-01-17 16:58:43 +0800

    changed some code

--- app/javascript/components/Body.js
+++ app/javascript/components/Body.js
@@ -99,14 +99,14 @@ class Body extends React.Component {
     // };
     // var response = SendGrid.send(msg);
     // console.log(response);
-    var helper = require("@sendgrid").mail;
+    var helper = require("@sendgrid/mail");
     var from_email = new helper.Email("test@example.com");
     var to_email = new helper.Email("e0406483@u.nus.edu");
     var subject = "Hello World from the SendGrid Node.js Library!";
     var content = new helper.Content("text/plain", "Hello, Email!");
     var mail = new helper.Mail(from_email, subject, to_email, content);
 
-    var sg = require("@sendgrid")(process.env.REACT_APP_SENDGRID_API_KEY);
+    var sg = require("@sendgrid/mail")(process.env.REACT_APP_SENDGRID_API_KEY);
     var request = sg.emptyRequest({
       method: "POST",
       path: "/v3/mail/send",
