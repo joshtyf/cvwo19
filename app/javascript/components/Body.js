451f4cfa8bae2574ac3f7f838cdfec0441004937
joshtyf <tan.yin.feng@hotmail.com>
2020-01-17 15:08:56 +0800

    testing sendgrid

--- app/javascript/components/Body.js
+++ app/javascript/components/Body.js
@@ -86,12 +86,38 @@ class Body extends React.Component {
   }
 
   handleRemind(data) {
-    console.log(data);
+    // console.log(data);
     // data = JSON.parse(data);
-    const sg = require("@sendgrid/mail");
-    sg.setApiKey(process.env.SENDGRID_API_KEY);
-    var response = sg.send(data);
-    console.log(response);
+    // SendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+    // const msg = {
+    //   to: "e0406483@u.nus.edu",
+    //   from: "test@example.com",
+    //   subject: "Sending with SendGrid is Fun",
+    //   text: "and easy to do anywhere, even with Node.js",
+    //   html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+    // };
+    // var response = SendGrid.send(msg);
+    // console.log(response);
+    var helper = require("sendgrid").mail;
+    var from_email = new helper.Email("e0406483@u.nus.edu");
+    var to_email = new helper.Email("e0406483@u.nus.edu");
+    var subject = "Hello World from the SendGrid Node.js Library!";
+    var content = new helper.Content("text/plain", "Hello, Email!");
+    var mail = new helper.Mail(from_email, subject, to_email, content);
+
+    var sg = require("sendgrid")(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: "POST",
+      path: "/v3/mail/send",
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
+    });
   }
 
   render() {
