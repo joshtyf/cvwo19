e680729d556a2da889ca16489b60fe6577271462
joshtyf <tan.yin.feng@hotmail.com>
2021-06-04 15:11:51 +0800

    Add function to post new todo to rails

--- app/javascript/utilities/index.js
+++ app/javascript/utilities/index.js
@@ -3,3 +3,16 @@ import axios from "axios";
 export function getTodos() {
   return axios.get("http://localhost:3000/tasks").then((res) => res.data);
 }
+
+export function createNewTask(title, description) {
+  const token = document.querySelector("meta[name=csrf-token]").content;
+  return axios.post(
+    "http://localhost:3000/tasks",
+    {
+      title: title,
+      description: description,
+      completed: false,
+    },
+    { headers: { "X-CSRF-Token": token } }
+  );
+}
