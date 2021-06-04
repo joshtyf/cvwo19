ed55e4a2c2a04848520d5d6d6761e6c9acc9d8ff
joshtyf <tan.yin.feng@hotmail.com>
2021-06-04 20:07:09 +0800

    Import bootstrap css

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,6 +5,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../components/app";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
