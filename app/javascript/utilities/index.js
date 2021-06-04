93a5d478fc3b81e8f4e80bc65a641d75d4fe9650
joshtyf <tan.yin.feng@hotmail.com>
2021-06-04 20:43:22 +0800

    Refactor code

--- app/javascript/utilities/index.js
+++ app/javascript/utilities/index.js
@@ -1,33 +1,25 @@
 import axios from "axios";
 
 const token = document.querySelector("meta[name=csrf-token]").content;
+axios.defaults.headers.common["X-CSRF-Token"] = token;
+const apiUrl = "http://localhost:3000/tasks";
 
 export function getTodos() {
-  return axios.get("http://localhost:3000/tasks").then((res) => res.data);
+  return axios.get(apiUrl).then((res) => res.data);
 }
 
 export function createNewTask(title, description) {
-  return axios.post(
-    "http://localhost:3000/tasks",
-    {
-      title: title,
-      description: description,
-      completed: false,
-    },
-    { headers: { "X-CSRF-Token": token } }
-  );
-}
-
-export function deleteTask(id) {
-  return axios.delete(`http://localhost:3000/tasks/${id}`, {
-    headers: { "X-CSRF-Token": token },
+  return axios.post(apiUrl, {
+    title: title,
+    description: description,
+    completed: false,
   });
 }
 
+export function deleteTask(id) {
+  return axios.delete(apiUrl + `/${id}`, {});
+}
+
 export function updateTask(id) {
-  return axios.put(
-    `http://localhost:3000/tasks/${id}`,
-    {},
-    { headers: { "X-CSRF-Token": token } }
-  );
+  return axios.put(apiUrl + `/${id}`, {});
 }
