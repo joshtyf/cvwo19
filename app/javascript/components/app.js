1ae219d489da100373a453bedcf509fa8164c076
joshtyf <tan.yin.feng@hotmail.com>
2021-06-04 12:24:39 +0800

    Implement the basic functionalities

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import { getTodos } from "../utilities/index";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [Todos, setTodos] = React.useState("");
+  React.useEffect(() => {
+    getTodos()
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h1>hi, this is the app!</h1>
+    <div className="container">
+      <div className="row">
+        <h1 className="text-center">Todo List</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
